c1dd5622696c211ee5ee445a98c954d4785f7a48
Owlsception <165500904+OwlSception@users.noreply.github.com>
2025-09-19 16:42:00 -0700

    Added update requirements function

--- utils/utils.R
+++ utils/utils.R
@@ -9,3 +9,13 @@ find_data_file <- function(base_dir, filename) {
     return(NULL)
   }
 }
+
+def update_requirements(project_directory):
+    """Updates the requirements.txt file with the current packages."""
+    try:
+        req_path = os.path.join(project_directory, "docs", "requirements.txt")
+        with open(req_path, "w") as req_file:
+            subprocess.run(["pip", "freeze"], stdout=req_file)
+        print(f'Updated requirements in {req_path}')
+    except Exception as e:
+        print(f'Error updating requirements: {e}')
